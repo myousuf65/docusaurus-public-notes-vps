915c9e5d58b4f3bb5a3e8946fafd033effc3861f
ypathan <ypathan@ypathans-MacBook-Air.local>
2024-07-06 23:49:53 +0800

    change github edit url

--- public-notes/docusaurus.config.js
+++ public-notes/docusaurus.config.js
@@ -44,14 +44,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/myousuf65/docusaurus-public-notes-vps/tree/main/public-notes/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/myousuf65/docusaurus-public-notes-vps/tree/main/public-notes/',
         },
         theme: {
           customCss: './src/css/custom.css',
